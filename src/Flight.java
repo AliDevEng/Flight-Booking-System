a6fd23772af114d6fdade8a80918f9dd3987fe24
Ali Rezai <ali.rezai.developer@gmail.com>
2025-03-28 08:11:09 +0100

    Created Fields in Flight-class

---
+++ src/Flight.java
@@ -0,0 +1,15 @@
+// All flight function
+
+public class Flight {
+
+    private String flightNumber;
+    private String origin;
+    private String destination;
+    private String departureDate;
+    private String departureTime;
+    private int totalSeats;
+    private int availableSeats;
+    private double price;
+
+
+}
