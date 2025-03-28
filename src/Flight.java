f3bc9d58aeef20d8ad58d443ac2d722b1322886f
Ali Rezai <ali.rezai.developer@gmail.com>
2025-03-28 08:18:52 +0100

    Created constructor in Flight to create a new flight-object

--- src/Flight.java
+++ src/Flight.java
@@ -11,5 +11,21 @@ public class Flight {
     private int availableSeats;
     private double price;
 
+    // We need a constructor to creating a new flight-object
+    public Flight
+    (String flightNumber, String origin, String destination, String departureDate, String departureTime, int totalSeats, double price) {
+        this.flightNumber = flightNumber;
+        this.origin = origin;
+        this. destination = destination;
+        this.departureDate = departureDate;
+        this.departureTime = departureTime;
+        this.totalSeats = totalSeats;
+        this.availableSeats = totalSeats;       // All seats are available at the beginning
+        this.price = price;
+    }
 
+    // We need some Getters to create our object
+    public String getFlightNumber() {
+        return flightNumber;
+    }
 }
